dc3905215bd089312f5534201975dbe750c0ac3a
Paul Gammans <pgammans@gammascience.co.uk>
2019-06-07 15:25:49 +0100

    Use solution message to bring solution into view from note click

--- src/app/solution/solution.component.ts
+++ src/app/solution/solution.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef  } from '@angular/core';
 import { Solution } from '../solution.model';
+import { timer } from 'rxjs';
+import { tap, switchMap, filter } from 'rxjs/operators';
+
+const timerdelay = 1000;
 
 @Component({
   selector: 'app-solution',
@@ -8,10 +12,29 @@ import { Solution } from '../solution.model';
 })
 export class SolutionComponent implements OnInit {
 
+  private reveal = false;
+
   @Input() model: Solution;
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
+    this.model.identify_watcher().pipe(
+      tap( sol => {
+        this.reveal = true;
+        if ( this.elementRef.nativeElement.scrollIntoViewIfNeeded !== undefined){
+          this.elementRef.nativeElement.scrollIntoViewIfNeeded(false);
+        } else {
+          this.elementRef.nativeElement.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+          });
+        }
+      }),
+      switchMap( (sol) => timer(timerdelay) ),
+      filter( v => v === 0 ),
+    ).subscribe( sol => {
+      this.reveal = false;
+    });
   }
   public toggleActive(event: Event) {
       if (event) {
